ebb436b27cd373165829c029522c1bbed6a4732b
V-edika <vedika100807@gmail.com>
2026-06-07 00:20:43 +0530

    Create first and last position of element.cpp

---
+++ week2 /first and last position of element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = -1, last = -1;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+            if (nums[mid] == target)
+                first = mid;
+        }
+        l = 0;  r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+            if (nums[mid] == target)
+                last = mid;
+        }
+        return {first, last};
+    }
+};
